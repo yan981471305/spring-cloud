c7cd3b4413af76975fb30a19a42ddc9b91f9dca9
LAPTOP-NMAR3S10\98147 <981471305@qq.com>
2019-04-29 09:20:41 +0800

    修改

--- eureka/src/main/java/demo/eureka/EurekaApplication.java
+++ eureka/src/main/java/demo/eureka/EurekaApplication.java
@@ -9,39 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
 
-  /*  @Value("${slack.url}")
-    String slackUrl;
-
-    @Value("${slack.channel}")
-    String slackChannel;
-
-    @Value("${slack.user}")
-    String slackUser;
-
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-    @EventListener(condition = "#eurekaInstanceCanceledEvent.replication==false")
-    public void listenCanceled(EurekaInstanceCanceledEvent eurekaInstanceCanceledEvent){
-        String message = String.format("Instance(%s) of service(%s) is DOWN at %s",
-                eurekaInstanceCanceledEvent.getServerId(),
-                eurekaInstanceCanceledEvent.getAppName(),
-                sdf.format(new Date()));
-        SlackApi api = new SlackApi(slackUrl);
-        api.call(new SlackMessage(slackChannel,slackUser,message));
-    }
-
-    @EventListener(condition = "#eurekaInstanceRegisteredEvent.replication==false")
-    public void listenRegistry(EurekaInstanceRegisteredEvent eurekaInstanceRegisteredEvent){
-        if(eurekaInstanceRegisteredEvent.getInstanceInfo().getStatus() == InstanceInfo.InstanceStatus.UP){
-            String message = String.format("Instance(%s) of service(%s) is UP at %s",
-                    eurekaInstanceRegisteredEvent.getInstanceInfo().getInstanceId(),
-                    eurekaInstanceRegisteredEvent.getInstanceInfo().getAppName(),
-                    sdf.format(new Date()));
-            SlackApi api = new SlackApi(slackUrl);
-            api.call(new SlackMessage(slackChannel,slackUser,message));
-        }
-    }*/
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
--- user-service/src/main/java/demo/ms/user/UserServiceApplication.java
+++ user-service/src/main/java/demo/ms/user/UserServiceApplication.java
@@ -1,6 +1,6 @@
 package demo.ms.user;
 
-import demo.ms.common.config.ResourceServerConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,13 +8,11 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @EnableHystrix
 @EnableCircuitBreaker
 @EntityScan({"demo.ms.user.entity"})
 @EnableEurekaClient
-@Import(ResourceServerConfig.class)
 @SpringBootApplication
 @Configuration
 public class UserServiceApplication {
