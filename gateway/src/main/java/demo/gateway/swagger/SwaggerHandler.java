d3dd866a18ad3f3d21c09ef6a147d6abb8c8ebe8
LAPTOP-NMAR3S10\98147 <981471305@qq.com>
2019-05-10 15:13:58 +0800

    网关整合swagger2

--- gateway/src/main/java/demo/gateway/swagger/SwaggerHandler.java
+++ gateway/src/main/java/demo/gateway/swagger/SwaggerHandler.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
